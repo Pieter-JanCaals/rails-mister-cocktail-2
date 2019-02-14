6a284720b07464f668ea8c10b622be9b9da89c63
Pieter-Jan Caals <Pieter.Jan.Caals@gmail.com>
2019-02-14 12:04:50 -0500

    Implemented all validations and associations

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,4 +3,5 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
 
   validates :description, :cocktail, :ingredient, presence: true
+  validates :ingredient, uniqueness: {scope: :cocktail}
 end
