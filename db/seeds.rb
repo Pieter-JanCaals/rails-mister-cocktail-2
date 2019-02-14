6146d8ba4b1dfb23a3ca7d979c363e1b87fd73a4
Pieter-Jan Caals <Pieter.Jan.Caals@gmail.com>
2019-02-14 14:52:14 -0500

    Implemented CRUD for dose

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,11 @@
 require ('open-uri')
+Dose.delete_all
+Ingredient.delete_all
+Cocktail.delete_all
 
 ingredients = JSON.parse(open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read)["drinks"]
 
-ingredient1 = Ingredient.create(
-  name: ingredients.sample["strIngredient1"]
-)
-
-ingredient2 = Ingredient.create(
-  name: ingredients.sample["strIngredient1"]
-)
-
-ingredient3 = Ingredient.create(
-  name: ingredients.sample["strIngredient1"]
-)
+ingredients.each { |ingredient| Ingredient.create(name: ingredient["strIngredient1"])}
 
 cocktail1 = Cocktail.create(
   name: Faker::FunnyName.three_word_name
@@ -24,36 +17,37 @@ cocktail2 = Cocktail.create(
 
 dose1 = Dose.create(
   cocktail: cocktail1,
-  ingredient: ingredient1,
-  description: "Add 2ml"
+  description: "Add 2ml",
+  ingredient: Ingredient.first
 )
+dose1.ingredient = Ingredient.first
 
 dose2 = Dose.create(
   cocktail: cocktail1,
-  ingredient: ingredient2,
-  description: "Add 2ml"
+  description: "Add 2ml",
+  ingredient: Ingredient.second
 )
 
 dose3 = Dose.create(
   cocktail: cocktail1,
-  ingredient: ingredient3,
-  description: "Add 2ml"
+  description: "Add 2ml",
+  ingredient: Ingredient.third
 )
 
 dose4 = Dose.create(
   cocktail: cocktail2,
-  ingredient: ingredient1,
-  description: "Add 2ml"
+  description: "Add 2ml",
+  ingredient: Ingredient.first
 )
 
 dose5 = Dose.create(
   cocktail: cocktail2,
-  ingredient: ingredient2,
-  description: "Add 2ml"
+  description: "Add 2ml",
+  ingredient: Ingredient.second
 )
 
 dose6 = Dose.create(
   cocktail: cocktail2,
-  ingredient: ingredient3,
-  description: "Add 2ml"
+  description: "Add 2ml",
+  ingredient: Ingredient.third
 )
