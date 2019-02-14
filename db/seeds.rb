7a1e79b6e82459ebc3949d9e1f30754e0a604715
Pieter-Jan Caals <Pieter.Jan.Caals@gmail.com>
2019-02-14 12:35:40 -0500

    Seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,59 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require ('open-uri')
+
+ingredients = JSON.parse(open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read)["drinks"]
+
+ingredient1 = Ingredient.create(
+  name: ingredients.sample["strIngredient1"]
+)
+
+ingredient2 = Ingredient.create(
+  name: ingredients.sample["strIngredient1"]
+)
+
+ingredient3 = Ingredient.create(
+  name: ingredients.sample["strIngredient1"]
+)
+
+cocktail1 = Cocktail.create(
+  name: Faker::FunnyName.three_word_name
+)
+
+cocktail2 = Cocktail.create(
+  name: Faker::FunnyName.three_word_name
+)
+
+dose1 = Dose.create(
+  cocktail: cocktail1,
+  ingredient: ingredient1,
+  description: "Add 2ml"
+)
+
+dose2 = Dose.create(
+  cocktail: cocktail1,
+  ingredient: ingredient2,
+  description: "Add 2ml"
+)
+
+dose3 = Dose.create(
+  cocktail: cocktail1,
+  ingredient: ingredient3,
+  description: "Add 2ml"
+)
+
+dose4 = Dose.create(
+  cocktail: cocktail2,
+  ingredient: ingredient1,
+  description: "Add 2ml"
+)
+
+dose5 = Dose.create(
+  cocktail: cocktail2,
+  ingredient: ingredient2,
+  description: "Add 2ml"
+)
+
+dose6 = Dose.create(
+  cocktail: cocktail2,
+  ingredient: ingredient3,
+  description: "Add 2ml"
+)
